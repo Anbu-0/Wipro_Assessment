87f12d0acd1841620ba898d3ceb19b252ede5f31
Anbu-0 <anbusakthiseenivasan@gmail.com>
2026-07-07 09:38:39 +0530

    Java fundamentals mini project

---
+++ Miniproject/Fundamentals.java
@@ -0,0 +1,78 @@
+package javafundamentals;
+
+public class Fundamentals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (args.length == 0) {
+            System.out.println("Please provide an employee id as a command line argument.");
+            System.out.println("Usage: java Project1 <empid>");
+            return;
+        }
+		String searchId = args[0];
+		
+		String[][] employees = {
+	            {"1001", "Ashish", "01/04/2009", "e", "R&D", "20000", "8000", "3000"},
+	            {"1002", "Sushma", "23/08/2012", "c", "PM", "30000", "12000", "9000"},
+	            {"1003", "Rahul", "12/11/2008", "k", "Acct", "10000", "8000", "1000"},
+	            {"1004", "Chahat", "29/01/2013", "r", "Front Desk", "12000", "6000", "2000"},
+	            {"1005", "Ranjan", "16/07/2005", "m", "Engg", "50000", "20000", "20000"},
+	            {"1006", "Suman", "1/1/2000", "e", "Manufacturing", "23000", "9000", "4400"},
+	            {"1007", "Tanmay", "12/06/2006", "c", "PM", "29000", "12000", "10000"}
+	        };
+		boolean isFound = false;
+		for (String[] emp : employees) {
+            if (emp[0].equals(searchId)) {
+                isFound = true;
+                
+                String empName = emp[1];
+                String desigCode = emp[3];
+                String department = emp[4];
+                int basic = Integer.parseInt(emp[5]);
+                int hra = Integer.parseInt(emp[6]);
+                int it = Integer.parseInt(emp[7]);
+
+                String designation = "";
+                int da = 0;
+                
+                switch (desigCode) {
+                case "e":
+                    designation = "Engineer";
+                    da = 20000;
+                    break;
+                case "c":
+                    designation = "Consultant";
+                    da = 32000;
+                    break;
+                case "k":
+                    designation = "Clerk";
+                    da = 12000;
+                    break;
+                case "r":
+                    designation = "Receptionist";
+                    da = 15000;
+                    break;
+                case "m":
+                    designation = "Manager";
+                    da = 40000;
+                    break;
+                default:
+                    designation = "Unknown";
+                    da = 0;
+                    break;
+            }
+                int salary = basic + hra + da - it;
+                
+                System.out.printf("%-10s %-12s %-14s %-15s %-10s\n", "Emp No.", "Emp Name", "Department", "Designation", "Salary");
+                System.out.printf("%-10s %-12s %-14s %-15s %-10d\n", searchId, empName, department, designation, salary);
+                
+                break;
+            }
+		}
+		if (!isFound) {
+            System.out.println("There is no employee with empid : " + searchId);
+        }
+
+	}
+
+}
